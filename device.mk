4a5936043370b569df3ba7553a1981ab4836c021
Gagan Malvi <malvigagan@gmail.com>
2021-06-10 07:18:06 +0200

    2e: Update thermal HAL to 2.0
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -155,8 +155,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-impl \
+    android.hardware.thermal@2.0-service
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
