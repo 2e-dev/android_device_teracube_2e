2a017fb341d530878b49b5ee730e436c9edcfbc5
Gagan Malvi <malvigagan@gmail.com>
2021-08-01 13:00:37 +0200

    2e: Remove useless NFC permission XMLs.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -275,12 +275,6 @@ PRODUCT_PACKAGES += \
     ueventd.mtk.rc
 
 # NFC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc-st/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc-st/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc-st/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc-st/android.hardware.nfc.uicc.xml
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
